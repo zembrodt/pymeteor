823a5a1610d5885176f0ba6c199e68dedaa1f103
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-10-11 22:30:22 -0400

    Added more documentation within code

--- pymeteor/pymeteor.py
+++ pymeteor/pymeteor.py
@@ -1,11 +1,9 @@
-"""
-PyMETEOR
-Python implementation of METEOR.
-"""
+# PyMETEOR
+# Python implementation of METEOR.
 
-from numpy.linalg import det
 import copy
 import random
+from numpy.linalg import det
 
 # Defines a Cartesian point
 class Point:
@@ -57,7 +55,7 @@ class Line:
         return True
 
 # Find all alignments between two sentences
-def count_intersections(lines):
+def _count_intersections(lines):
     intersection_count = 0
     for i, line1 in enumerate(lines):
         j = i+1
@@ -68,34 +66,36 @@ def count_intersections(lines):
     return intersection_count
 
 # Iterate over each one, count number of assignments
-def create_map(r, c):
-    m = {}
-    for i in range(len(r)):
-        for j in range(len(c)):
-            if r[i] == c[j]:
-                if i in m:
-                    m[i].append(j)
+def _create_map(reference, candidate):
+    _map = {}
+    for i in range(len(reference)):
+        for j in range(len(candidate)):
+            if reference[i] == candidate[j]:
+                if i in _map:
+                    _map[i].append(j)
                 else:
-                    m[i] = [j]
-    return m
+                    _map[i] = [j]
+    return _map
 
-def generate_alignments(m, keys):
+# Recursive function
+# Generates all possible alignments between the candidate and reference strings passed in _create_alignments
+def _generate_alignments(_map, keys):
     alignments = []
     curr_alignment = []
     # Used for if we are in a recursive call, we want to remove any duplicates of the inital value
-    if len(keys) > 1 and len(m[keys[0]]) == 1:
-        value = m[keys[0]][0]
+    if len(keys) > 1 and len(_map[keys[0]]) == 1:
+        value = _map[keys[0]][0]
         for key in keys[1:]:
-            if value in m[key]:
-                m[key].remove(value)
+            if value in _map[key]:
+                _map[key].remove(value)
     for i, key in enumerate(keys):
-        if len(m[key]) == 1:
-            curr_alignment.append(Line(Point(key,0), Point(m[key][0],1)))
-        elif len(m[key]) > 1:
-            for v in m[key]:
-                m_temp = copy.deepcopy(m)
-                m_temp[key] = [v]
-                temp_alignments = generate_alignments(m_temp, keys[i:])
+        if len(_map[key]) == 1:
+            curr_alignment.append(Line(Point(key,0), Point(_map[key][0],1)))
+        elif len(_map[key]) > 1:
+            for v in _map[key]:
+                map_temp = copy.deepcopy(_map)
+                map_temp[key] = [v]
+                temp_alignments = _generate_alignments(map_temp, keys[i:])
                 for alignment in temp_alignments:
                     alignments.append(curr_alignment + alignment)
             return alignments
@@ -103,14 +103,16 @@ def generate_alignments(m, keys):
         alignments.append(curr_alignment)
     return alignments
 
-def create_alignments(reference, candidate):
-    m = create_map(reference.split(), candidate.split())
-    return generate_alignments(m, list(m.keys()))
+# Starts initial call of _generate_alignments
+# Maps the reference and candidate strings into indices and generates all their possible alignments
+def _create_alignments(reference, candidate):
+    _map = _create_map(reference.split(), candidate.split())
+    return _generate_alignments(_map, list(_map))
 
 # Input:  reference, candidate strings
 # Output: chunks, mappings
-def calculate_chunks(reference, candidate):
-    alignments = create_alignments(reference, candidate)
+def _calculate_chunks(reference, candidate):
+    alignments = _create_alignments(reference, candidate)
 
     # Return the alignment with lowest assignment number
     min_alignments = []
@@ -129,7 +131,7 @@ def calculate_chunks(reference, candidate):
         min_intersections = None
         min_intersection_alignments = []
         for alignment in min_alignments:
-            intersections = count_intersections(alignment)
+            intersections = _count_intersections(alignment)
             if min_intersections is None or intersections < min_intersections:
                 min_intersections = intersections
                 min_intersection_alignments = [alignment]
@@ -155,9 +157,8 @@ def calculate_chunks(reference, candidate):
         prev_val = line.point2.x
     return chunks, mappings
 
-
 # m: number of unigrams in the candidate translation also found in the reference translation
-def calculate_m(reference_unigrams, candidate_unigrams):
+def _calculate_m(reference_unigrams, candidate_unigrams):
     m = 0
     for unigram in candidate_unigrams:
         if unigram in reference_unigrams:
@@ -166,42 +167,52 @@ def calculate_m(reference_unigrams, candidate_unigrams):
 
 # m: number of unigrams in the candidate translation also found in the reference translation
 # wt: number of unigrams in the candidate translation
-def unigram_precision(reference, candidate):
+def _unigram_precision(reference, candidate):
     candidate_unigrams = candidate.split()
-    m = calculate_m(reference.split(), candidate_unigrams)
+    m = _calculate_m(reference.split(), candidate_unigrams)
     wt = len(candidate_unigrams)
     
     return m / float(wt)
 
 # m: number of unigrams in the candidate translation also found in the reference translation
 # wr: number of unigrams in the reference translation
-def unigram_recall(reference, candidate):
+def _unigram_recall(reference, candidate):
     reference_unigrams = reference.split()
-    m = calculate_m(reference_unigrams, candidate.split())
+    m = _calculate_m(reference_unigrams, candidate.split())
     wr = len(reference_unigrams)
 
     return m / float(wr)
 
-# F_mean
-def harmonic_mean(p, r):
-    if p == 0 and r == 0:
+
+# P: Unigram precision
+# R: Unigram recall
+# Output: F_mean
+def _harmonic_mean(P, R):
+    if P == 0 and R == 0:
         return 0
     else:
-        return (10*p*r) / float(r+9*p)
+        return (10*P*R) / float(R+9*P)
 
 # c: number of chunks
 # um: number of unigrams that have been mapped
-def calculate_penalty(reference, candidate, ret_details=False):
-    chunks, mappings = calculate_chunks(reference, candidate)
+def _calculate_penalty(reference, candidate, ret_details=False):
+    chunks, mappings = _calculate_chunks(reference, candidate)
     penalty_val = 0.5 * pow((chunks/float(mappings)), 3)
     if not ret_details:
         return (penalty_val,)
     else:
         return penalty_val, chunks, mappings
 
-def calculate_meteor(f_mean, penalty):
+# f_mean: the harmonic_mean
+# penalty: the penalty calculated within calculate_penalty)
+def _calculate_meteor(f_mean, penalty):
     return f_mean * (1 - penalty)
 
+# reference: the reference string, with each unigram separated by white space
+# candidate: the candidate string, with each unigram separated by white space
+# print_details: if the user wishes to receive more information on the METEOR score as stdout (default is False)
+# Output: the METEOR score as a float
+#   Note: if neither reference or candidate strings have a matching unigram, the score is 0
 def meteor(reference, candidate, print_details=False):
     if print_details:
         print('Reference: "%s"' % reference)
@@ -211,15 +222,15 @@ def meteor(reference, candidate, print_details=False):
     candidate_set = set(candidate.split())
     if len(reference_set.intersection(candidate_set)) == 0:
         if print_details:
-            print('Reference and candidate sentences have no matching words')
+            print('Reference and candidate sentences have no matching unigrams')
         return 0.0
     
-    P = unigram_precision(reference, candidate)
-    R = unigram_recall(reference, candidate)
-    f_mean = harmonic_mean(P, R)
-    penalty = calculate_penalty(reference, candidate, ret_details=print_details)
+    P = _unigram_precision(reference, candidate)
+    R = _unigram_recall(reference, candidate)
+    f_mean = _harmonic_mean(P, R)
+    penalty = _calculate_penalty(reference, candidate, ret_details=print_details)
     penalty_val = penalty[0]
-    M = calculate_meteor(f_mean, penalty_val)
+    M = _calculate_meteor(f_mean, penalty_val)
     
     if print_details:
         chunks = penalty[1]
