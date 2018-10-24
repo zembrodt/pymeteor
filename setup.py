a334dce2a3cba5a9d6a9fe3f377fc23bcbcc3e1d
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-10-24 01:01:28 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymeteor",
+    version="0.0.2",
+    author="Ryan Zembrodt",
+    author_email="ryan.zembrodt@uky.edu",
+    description="Python implementation of METEOR",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zembrodt/pymeteor",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'nose'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
